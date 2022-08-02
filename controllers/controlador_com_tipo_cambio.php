d345bfc223fdda7ccae0a0348dfd630e3991e21a
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-02 10:39:28 -0500

    v0.6.0 Se integra lata base para tipo de comabio

---
+++ controllers/controlador_com_tipo_cambio.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace gamboamartin\comercial\controllers;
+
+use gamboamartin\system\links_menu;
+use gamboamartin\system\system;
+use gamboamartin\template\html;
+use html\com_tipo_cambio_html;
+use models\com_tipo_cambio;
+use PDO;
+use stdClass;
+
+class controlador_com_tipo_cambio extends system {
+
+    public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(), stdClass $paths_conf = new stdClass()){
+        $modelo = new com_tipo_cambio(link: $link);
+
+        $html_ = new com_tipo_cambio_html(html: $html);
+        $obj_link = new links_menu($this->registro_id);
+        parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
+
+        $this->titulo_lista = 'Tipos de cambio';
+
+    }
+
+
+
+
+
+}
