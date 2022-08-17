48e6a87ff609983b24b3f609725278359002780e
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-17 13:15:04 -0500

    v0.18.5 Se integra lebl a sucursal cliente select

--- templates/directivas/com_sucursal_html.php
+++ templates/directivas/com_sucursal_html.php
@@ -253,12 +253,13 @@ class com_sucursal_html extends html_controler {
         return $selects;
     }
 
-    public function select_com_sucursal_id(int $cols, bool $con_registros, int $id_selected, PDO $link): array|string
+    public function select_com_sucursal_id(int $cols, bool $con_registros, int $id_selected, PDO $link,
+                                           string $label ='Sucursal'): array|string
     {
         $modelo = new com_sucursal(link: $link);
-
+        
         $select = $this->select_catalogo(cols:$cols,con_registros:$con_registros,id_selected:$id_selected,
-            modelo: $modelo,label: 'Sucursal',required: true);
+            modelo: $modelo,label: $label,required: true);
         if(errores::$error){
             return $this->error->error(mensaje: 'Error al generar select', data: $select);
         }
