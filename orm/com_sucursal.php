102c8ccd63f7ec918c0b457419846045e81784f3
martingamboaivitec <martin.gamboa@ivitec.com.mx>
2022-08-18 16:04:45 -0500

    v0.20.5 Se integra com cliente a sucursal

--- orm/com_sucursal.php
+++ orm/com_sucursal.php
@@ -7,7 +7,7 @@ class com_sucursal extends modelo{
 
     public function __construct(PDO $link){
         $tabla = __CLASS__;
-        $columnas = array($tabla=>false);
+        $columnas = array($tabla=>false,'com_cliente'=>$tabla);
         $campos_obligatorios = array();
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
