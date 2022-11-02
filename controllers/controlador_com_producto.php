2b9835c976cc20369d73b4931563d306394afc0f
[user] <angel.martinez@ivitec.com.mx>
2022-11-02 12:38:48 -0600

    v0.49.12 Se ajustan links menu

--- controllers/controlador_com_producto.php
+++ controllers/controlador_com_producto.php
@@ -23,7 +23,7 @@ class controlador_com_producto extends system {
                                 stdClass $paths_conf = new stdClass()){
         $modelo = new com_producto(link: $link);
         $html_ = new com_producto_html(html: $html);
-        $obj_link = new links_menu($this->registro_id);
+        $obj_link = new links_menu(link: $link,registro_id: $this->registro_id);
         parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
 
         $this->titulo_lista = 'Sucursal';
--- controllers/controlador_com_tipo_producto.php
+++ controllers/controlador_com_tipo_producto.php
@@ -24,7 +24,7 @@ class controlador_com_tipo_producto extends system {
         $modelo = new com_tipo_cambio(link: $link);
 
         $html_ = new com_tipo_cambio_html(html: $html);
-        $obj_link = new links_menu($this->registro_id);
+        $obj_link = new links_menu(link: $link,registro_id: $this->registro_id);
         parent::__construct(html:$html_, link: $link,modelo:  $modelo, obj_link: $obj_link, paths_conf: $paths_conf);
 
         $this->titulo_lista = 'Tipos de cambio';
