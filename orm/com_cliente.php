0c6e0d4db01880fddfc1b9ef0a17c7ab0cce2d00
Kevin <kevin.acuna@ivitec.com.mx>
2022-11-17 15:41:12 -0500

    v0.55.12 Se ajusta cliente

--- orm/com_cliente.php
+++ orm/com_cliente.php
@@ -7,8 +7,10 @@ use gamboamartin\cat_sat\models\cat_sat_moneda;
 use gamboamartin\cat_sat\models\cat_sat_regimen_fiscal;
 use gamboamartin\cat_sat\models\cat_sat_tipo_de_comprobante;
 use gamboamartin\cat_sat\models\cat_sat_uso_cfdi;
+use gamboamartin\direccion_postal\models\dp_calle;
 use gamboamartin\direccion_postal\models\dp_calle_pertenece;
 use gamboamartin\direccion_postal\models\dp_colonia;
+use gamboamartin\direccion_postal\models\dp_colonia_postal;
 use gamboamartin\direccion_postal\models\dp_cp;
 use gamboamartin\direccion_postal\models\dp_estado;
 use gamboamartin\direccion_postal\models\dp_municipio;
@@ -20,183 +22,140 @@ use stdClass;
 class com_cliente extends modelo{
     public function __construct(PDO $link){
         $tabla = 'com_cliente';
-        $columnas = array($tabla=>false,'cat_sat_moneda'=>$tabla,'dp_pais'=>'cat_sat_moneda');
+        $columnas = array($tabla=>false,'cat_sat_moneda'=>$tabla,'dp_pais'=>'cat_sat_moneda',
+            'cat_sat_regimen_fiscal' => $tabla);
         $campos_obligatorios = array('cat_sat_moneda_id','cat_sat_regimen_fiscal_id','cat_sat_moneda_id',
             'cat_sat_forma_pago_id','cat_sat_uso_cfdi_id','cat_sat_tipo_de_comprobante_id','cat_sat_metodo_pago_id');
 
         $tipo_campos = array();
         $tipo_campos['rfc'] = 'rfc';
 
-        $campos_view = array();
-        $campos_view['dp_pais_id']['type'] = 'selects';
-        $campos_view['dp_pais_id']['model'] = (new dp_pais($link));
+        $campos_view['dp_pais_id'] = array('type' => 'selects', 'model' => new dp_pais($link));
+        $campos_view['dp_estado_id'] = array('type' => 'selects', 'model' => new dp_estado($link));
+        $campos_view['dp_municipio_id'] = array('type' => 'selects', 'model' => new dp_municipio($link));
+        $campos_view['dp_cp_id'] = array('type' => 'selects', 'model' => new dp_cp($link));
+        $campos_view['dp_colonia_postal_id'] = array('type' => 'selects', 'model' => new dp_colonia_postal($link));
+        $campos_view['dp_calle_pertenece_id'] = array('type' => 'selects', 'model' => new dp_calle_pertenece($link));
+        $campos_view['cat_sat_regimen_fiscal_id'] = array('type' => 'selects', 'model' => new cat_sat_regimen_fiscal($link));
+        $campos_view['cat_sat_moneda_id'] = array('type' => 'selects', 'model' => new cat_sat_moneda($link));
+        $campos_view['cat_sat_forma_pago_id'] = array('type' => 'selects', 'model' => new cat_sat_forma_pago($link));
+        $campos_view['cat_sat_uso_cfdi_id'] = array('type' => 'selects', 'model' => new cat_sat_metodo_pago($link));
+        $campos_view['cat_sat_metodo_pago_id'] = array('type' => 'selects', 'model' => new cat_sat_uso_cfdi($link));
+        $campos_view['cat_sat_tipo_de_comprobante_id'] = array('type' => 'selects', 'model' => new cat_sat_tipo_de_comprobante($link));
+        $campos_view['codigo'] = array('type' => 'inputs');
+        $campos_view['razon_social'] = array('type' => 'inputs');
+        $campos_view['rfc'] = array('type' => 'inputs');
+        $campos_view['numero_exterior'] = array('type' => 'inputs');
+        $campos_view['numero_interior'] = array('type' => 'inputs');
+        $campos_view['telefono'] = array('type' => 'inputs');
 
-        $campos_view['dp_estado_id']['type'] = 'selects';
-        $campos_view['dp_estado_id']['model'] = (new dp_estado($link));
-
-        $campos_view['dp_municipio_id']['type'] = 'selects';
-        $campos_view['dp_municipio_id']['model'] = (new dp_municipio($link));
-
-        $campos_view['dp_cp_id']['type'] = 'selects';
-        $campos_view['dp_cp_id']['model'] = (new dp_cp($link));
-
-        $campos_view['dp_colonia_id']['type'] = 'selects';
-        $campos_view['dp_colonia_id']['model'] = (new dp_colonia($link));
-
-        $campos_view['dp_calle_pertenece_id']['type'] = 'selects';
-        $campos_view['dp_calle_pertenece_id']['model'] = (new dp_calle_pertenece($link));
-
-        $campos_view['cat_sat_regimen_fiscal_id']['type'] = 'selects';
-        $campos_view['cat_sat_regimen_fiscal_id']['model'] = (new cat_sat_regimen_fiscal($link));
-
-        $campos_view['cat_sat_moneda_id']['type'] = 'selects';
-        $campos_view['cat_sat_moneda_id']['model'] = (new cat_sat_moneda($link));
-
-        $campos_view['cat_sat_forma_pago_id']['type'] = 'selects';
-        $campos_view['cat_sat_forma_pago_id']['model'] = (new cat_sat_forma_pago($link));
-
-        $campos_view['cat_sat_metodo_pago_id']['type'] = 'selects';
-        $campos_view['cat_sat_metodo_pago_id']['model'] = (new cat_sat_metodo_pago($link));
-
-        $campos_view['cat_sat_uso_cfdi_id']['type'] = 'selects';
-        $campos_view['cat_sat_uso_cfdi_id']['model'] = (new cat_sat_uso_cfdi($link));
-
-        $campos_view['cat_sat_tipo_de_comprobante_id']['type'] = 'selects';
-        $campos_view['cat_sat_tipo_de_comprobante_id']['model'] = (new cat_sat_tipo_de_comprobante($link));
-
-        $campos_view['razon_social']['type'] = 'inputs';
-        $campos_view['rfc']['type'] = 'inputs';
-        $campos_view['numero_exterior']['type'] = 'inputs';
-        $campos_view['numero_interior']['type'] = 'inputs';
-        $campos_view['telefono']['type'] = 'inputs';
-
-
-        parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
-            columnas: $columnas, tipo_campos: $tipo_campos, campos_view: $campos_view);
+        parent::__construct(link: $link, tabla: $tabla, campos_obligatorios: $campos_obligatorios,
+            columnas: $columnas, campos_view: $campos_view, tipo_campos: $tipo_campos);
 
         $this->NAMESPACE = __NAMESPACE__;
     }
     public function alta_bd(): array|stdClass
     {
-
-        $registro = $this->descripcion_select(registro: $this->registro);
+        $this->registro = $this->init_base(data:$this->registro);
         if(errores::$error){
-            return $this->error->error(mensaje: 'Error al asignar descripcion select', data: $registro);
+            return $this->error->error(mensaje: 'Error al inicializar campo base',data: $this->registro);
         }
 
-        if(!isset($registro['cat_sat_moneda_id']) || $registro['cat_sat_moneda_id'] === -1){
-            $id_predeterminado = (new cat_sat_moneda($this->link))->id_predeterminado();
-            if(errores::$error){
-                return $this->error->error(mensaje: 'Error al obtener moneda predeterminada', data: $id_predeterminado);
-            }
-            $registro['cat_sat_moneda_id'] = $id_predeterminado;
-        }
-
-        if(!isset($registro['dp_calle_pertenece_id']) || $registro['dp_calle_pertenece_id'] === -1){
-            $id_predeterminado = (new dp_calle_pertenece($this->link))->id_predeterminado();
-            if(errores::$error){
-                return $this->error->error(mensaje: 'Error al obtener dp_calle_pertenece predeterminada', data: $id_predeterminado);
-            }
-            $registro['dp_calle_pertenece_id'] = $id_predeterminado;
-        }
-
-        if(!isset($registro['cat_sat_regimen_fiscal_id']) || $registro['cat_sat_regimen_fiscal_id'] === -1){
-            $id_predeterminado = (new cat_sat_regimen_fiscal($this->link))->id_predeterminado();
-            if(errores::$error){
-                return $this->error->error(mensaje: 'Error al obtener cat_sat_regimen_fiscal predeterminada', data: $id_predeterminado);
-            }
-            $registro['cat_sat_regimen_fiscal_id'] = $id_predeterminado;
-        }
-
-        if(!isset($registro['cat_sat_forma_pago_id']) || $registro['cat_sat_forma_pago_id'] === -1){
-            $id_predeterminado = (new cat_sat_forma_pago($this->link))->id_predeterminado();
-            if(errores::$error){
-                return $this->error->error(mensaje: 'Error al obtener cat_sat_forma_pago_id predeterminada', data: $id_predeterminado);
-            }
-            $registro['cat_sat_forma_pago_id'] = $id_predeterminado;
-        }
-
-        if(!isset($registro['cat_sat_uso_cfdi_id']) || $registro['cat_sat_uso_cfdi_id'] === -1){
-            $id_predeterminado = (new cat_sat_uso_cfdi($this->link))->id_predeterminado();
-            if(errores::$error){
-                return $this->error->error(mensaje: 'Error al obtener cat_sat_uso_cfdi predeterminada', data: $id_predeterminado);
-            }
-            $registro['cat_sat_uso_cfdi_id'] = $id_predeterminado;
-        }
-        if(!isset($registro['cat_sat_tipo_de_comprobante_id']) || $registro['cat_sat_tipo_de_comprobante_id'] === -1){
-            $id_predeterminado = (new cat_sat_tipo_de_comprobante($this->link))->id_predeterminado();
-            if(errores::$error){
-                return $this->error->error(mensaje: 'Error al obtener cat_sat_tipo_de_comprobante predeterminada', data: $id_predeterminado);
-            }
-            $registro['cat_sat_tipo_de_comprobante_id'] = $id_predeterminado;
-        }
-
-        if(!isset($registro['cat_sat_metodo_pago_id']) || $registro['cat_sat_metodo_pago_id'] === -1){
-            $id_predeterminado = (new cat_sat_metodo_pago($this->link))->id_predeterminado();
-            if(errores::$error){
-                return $this->error->error(mensaje: 'Error al obtener cat_sat_metodo_pago predeterminada', data: $id_predeterminado);
-            }
-            $registro['cat_sat_metodo_pago_id'] = $id_predeterminado;
-        }
-
-        if(isset($registro['dp_colonia_id'])){
-            unset($registro['dp_colonia_id']);
-        }
-
-
-
-        $this->registro = $registro;
-
-        $r_alta_bd = parent::alta_bd(); // TODO: Change the autogenerated stub
+        $this->registro = $this->inicializa_foraneas(data:$this->registro);
         if(errores::$error){
-            return $this->error->error(mensaje: 'Error al dar de alta registro', data: $r_alta_bd);
+            return $this->error->error(mensaje: 'Error al inicializar foraneas',data: $this->registro);
+        }
+
+        $this->registro = $this->limpia_campos(registro: $this->registro, campos_limpiar: array('dp_pais_id',
+            'dp_estado_id','dp_municipio_id','dp_cp_id','dp_cp_id','dp_colonia_postal_id'));
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al limpiar campos', data: $this->registro);
+        }
+
+        $r_alta_bd =  parent::alta_bd();
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al insertar cliente', data: $r_alta_bd);
         }
         return $r_alta_bd;
     }
 
-    /**
-     * Ajusta la descripcion select
-     * @param array $registro Registro en proceso de alta
-     * @return array
-     * @version 0.28.6
-     */
-    private function descripcion_select(array $registro): array
+    protected function init_base(array $data): array
     {
-        $keys = array('codigo','razon_social','rfc');
-        $valida = $this->validacion->valida_existencia_keys(keys:$keys, registro: $registro);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al validar registro', data: $valida);
+        if(!isset($data['descripcion'])){
+            $data['descripcion'] =  $data['razon_social'];
         }
-        if(!isset($registro['descripcion_select']) || $registro['descripcion_select'] === ''){
-            $registro['descripcion_select'] = $registro['codigo'];
-            $registro['descripcion_select'] .= ' '.$registro['rfc'];
-            $registro['descripcion_select'] .= ' '.$registro['razon_social'];
+
+        if(!isset($data['codigo_bis'])){
+            $data['codigo_bis'] =  $data['codigo'];
+        }
+
+        if(!isset($data['descripcion_select'])){
+            $ds = str_replace("_"," ",$data['descripcion']);
+            $ds = ucwords($ds);
+            $data['descripcion_select'] =  "{$data['codigo']} - {$ds}";
+        }
+
+        if(!isset($data['alias'])){
+            $data['alias'] = $data['codigo'];
+        }
+        return $data;
+    }
+
+    private function inicializa_foraneas(array $data): array
+    {
+        $foraneas['cat_sat_moneda_id'] = new cat_sat_moneda($this->link);
+        $foraneas['dp_calle_pertenece_id'] = new dp_calle_pertenece($this->link);
+        $foraneas['cat_sat_regimen_fiscal_id'] = new cat_sat_regimen_fiscal($this->link);
+        $foraneas['cat_sat_forma_pago_id'] = new cat_sat_forma_pago($this->link);
+        $foraneas['cat_sat_uso_cfdi_id'] = new cat_sat_tipo_de_comprobante($this->link);
+        $foraneas['cat_sat_tipo_de_comprobante_id'] = new cat_sat_uso_cfdi($this->link);
+        $foraneas['cat_sat_metodo_pago_id'] = new cat_sat_metodo_pago($this->link);
+
+        foreach ($foraneas as $key => $valor){
+            if(!isset($data[$key]) || $data[$key] === -1){
+                $predeterminado = ($valor)->id_predeterminado();
+                if(errores::$error){
+                    return $this->error->error(mensaje: "Error al $key predeterminada", data: $predeterminado);
+                }
+                $data[$key] = $predeterminado;
+            }
+        }
+        return $data;
+    }
+
+    private function limpia_campos(array $registro, array $campos_limpiar): array
+    {
+        foreach ($campos_limpiar as $valor) {
+            if (isset($registro[$valor])) {
+                unset($registro[$valor]);
+            }
         }
         return $registro;
     }
 
     public function modifica_bd(array $registro, int $id, bool $reactiva = false): array|stdClass
     {
-        var_dump($registro);
-
-        if(isset($registro['dp_pais_id'])){
-            unset($registro['dp_pais_id']);
+        $registro = $this->init_base(data:$registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al inicializar campo base',data: $registro);
         }
 
-        if(isset($registro['dp_estado_id'])){
-            unset($registro['dp_estado_id']);
-        }
-        if(isset($registro['dp_municipio_id'])){
-            unset($registro['dp_municipio_id']);
-        }
-        if(isset($registro['dp_cp_id'])){
-            unset($registro['dp_cp_id']);
-        }
-        if(isset($registro['dp_colonia_id'])){
-            unset($registro['dp_colonia_id']);
+        $registro = $this->inicializa_foraneas(data:$registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al inicializar foraneas',data: $registro);
         }
 
+        $registro = $this->limpia_campos(registro: $registro, campos_limpiar: array('dp_pais_id', 'dp_estado_id',
+            'dp_municipio_id','dp_cp_id','dp_cp_id','dp_colonia_postal_id'));
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al limpiar campos', data: $registro);
+        }
 
-        return parent::modifica_bd($registro, $id, $reactiva); // TODO: Change the autogenerated stub
+        $r_modifica_bd = parent::modifica_bd($registro, $id, $reactiva);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al modificar producto',data:  $r_modifica_bd);
+        }
+
+        return $r_modifica_bd;
     }
 }
