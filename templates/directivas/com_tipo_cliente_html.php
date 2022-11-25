96afc8f10b8ceae06da8bdcf32f82ff0f7a571f1
[user] <angel.martinez@ivitec.com.mx>
2022-11-25 17:24:42 -0600

    v0.62.14 Fix tipo cliente

--- templates/directivas/com_tipo_cliente_html.php
+++ templates/directivas/com_tipo_cliente_html.php
@@ -1,16 +1,16 @@
 <?php
 namespace html;
 
+use gamboamartin\comercial\models\com_tipo_cliente;
 use gamboamartin\errores\errores;
 use gamboamartin\system\html_controler;
-use models\com_tipo_cliente;
 use PDO;
 
 
 class com_tipo_cliente_html extends html_controler {
 
 
-    public function select_com_tipo_cambio_id(int $cols, bool $con_registros, int $id_selected, PDO $link): array|string
+    public function select_com_tipo_cliente_id(int $cols, bool $con_registros, int $id_selected, PDO $link): array|string
     {
         $modelo = new com_tipo_cliente(link: $link);
 
