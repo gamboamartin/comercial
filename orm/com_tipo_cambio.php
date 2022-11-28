6269a4379f8088fa96c26dc5b3855485d0c2260f
Kevin <kevin.acuna@ivitec.com.mx>
2022-11-28 12:48:42 -0500

    v0.64.14 Se ajusta tipo de producto

--- orm/com_tipo_cambio.php
+++ orm/com_tipo_cambio.php
@@ -1,16 +1,21 @@
 <?php
 namespace gamboamartin\comercial\models;
-use base\orm\modelo;
+use base\orm\_modelo_parent;
+use gamboamartin\cat_sat\models\cat_sat_moneda;
 use PDO;
 
-class com_tipo_cambio extends modelo{
+class com_tipo_cambio extends _modelo_parent{
     public function __construct(PDO $link){
         $tabla = 'com_tipo_cambio';
         $columnas = array($tabla=>false,'cat_sat_moneda'=>$tabla,'dp_pais'=>'cat_sat_moneda');
         $campos_obligatorios = array('cat_sat_moneda_id');
 
-        parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
-            columnas: $columnas);
+        $campos_view['cat_sat_moneda_id'] = array('type' => 'selects', 'model' => new cat_sat_moneda($link));
+        $campos_view['codigo'] = array('type' => 'inputs');
+        $campos_view['descripcion'] = array('type' => 'inputs');
+
+        parent::__construct(link: $link, tabla: $tabla, campos_obligatorios: $campos_obligatorios,
+            columnas: $columnas, campos_view: $campos_view);
 
         $this->NAMESPACE = __NAMESPACE__;
     }
