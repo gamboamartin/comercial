09097f1e9ec6984fc082d7baf4df2a528ae2af3f
Kevin <kevin.acuna@ivitec.com.mx>
2022-11-28 12:18:38 -0500

    v0.62.14 Se ajusto tipo de cliente

--- templates/inputs/com_tipo_cliente/alta.php
+++ templates/inputs/com_tipo_cliente/alta.php
@@ -1,5 +1,5 @@
 <?php /** @var controllers\controlador_dp_estado $controlador  controlador en ejecucion */ ?>
 <?php use config\views; ?>
-<?php echo $controlador->forms_inputs_alta; ?>
-
-<?php include (new views())->ruta_templates.'botons/submit/alta_bd_otro.php';?>
+<?php echo $controlador->inputs->codigo; ?>
+<?php echo $controlador->inputs->descripcion; ?>
+<?php include (new views())->ruta_templates.'botons/submit/alta_bd.php';?>
