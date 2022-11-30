0ec930eadf568576778112435306638318337188
Kevin <kevin.acuna@ivitec.com.mx>
2022-11-30 16:12:43 -0500

    v0.70.14 Se integra func. get_productos

--- controllers/controlador_com_producto.php
+++ controllers/controlador_com_producto.php
@@ -82,6 +82,18 @@ class controlador_com_producto extends system {
         }
     }
 
+    public function get_productos(bool $header, bool $ws = true): array|stdClass
+    {
+        $keys['com_tipo_producto'] = array('id','descripcion','codigo','codigo_bis');
+
+        $salida = $this->get_out(header: $header,keys: $keys, ws: $ws);
+        if(errores::$error){
+            return $this->retorno_error(mensaje: 'Error al generar salida',data:  $salida,header: $header,ws: $ws);
+        }
+
+        return $salida;
+    }
+
     private function inicializa_propiedades(): array
     {
         $identificador = "cat_sat_tipo_producto_id";
