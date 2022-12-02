3d1811a427ab850c4bf2eafd4be4733a7fbf29db
mgamboa <gamboa.martin@gmail.com>
2022-12-02 09:31:40 -0600

    v0.86.14 Seinetra tipo cambioo alta

--- controllers/controlador_cat_sat_moneda.php
+++ controllers/controlador_cat_sat_moneda.php
@@ -12,4 +12,11 @@ use PDO;
 use stdClass;
 
 
-class controlador_cat_sat_moneda extends \gamboamartin\cat_sat\controllers\controlador_cat_sat_moneda {}
+class controlador_cat_sat_moneda extends \gamboamartin\cat_sat\controllers\controlador_cat_sat_moneda {
+
+    public function tipos_de_cambio(bool $header, bool $ws){
+
+    }
+
+
+}
