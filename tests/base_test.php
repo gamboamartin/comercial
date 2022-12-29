781ecb9c2203ceb883a0c8ceb47d86dc6bfe5968
mgamboa <gamboa.martin@gmail.com>
2022-12-29 11:29:44 -0600

    v1.2.0 Se ajusta ut com tsuc

--- tests/base_test.php
+++ tests/base_test.php
@@ -11,6 +11,7 @@ use gamboamartin\comercial\models\com_producto;
 use gamboamartin\comercial\models\com_sucursal;
 use gamboamartin\comercial\models\com_tipo_cambio;
 use gamboamartin\comercial\models\com_tipo_cliente;
+use gamboamartin\comercial\models\com_tipo_sucursal;
 use gamboamartin\direccion_postal\models\dp_calle_pertenece;
 use gamboamartin\errores\errores;
 use PDO;
@@ -75,7 +76,7 @@ class base_test{
     public function alta_com_cliente(PDO $link, int $cat_sat_metodo_pago_id = 1, int $cat_sat_moneda_id = 1,
                                      int $cat_sat_regimen_fiscal_id = 1, int $cat_sat_tipo_de_comprobante_id = 1,
                                      int $cat_sat_uso_cfdi_id = 1, int $com_tipo_cliente_id = 1,
-                                     int $dp_calle_pertenece_id = 1): array|\stdClass
+                                     int $dp_calle_pertenece_id = 1, int $id = 1): array|\stdClass
     {
 
         $existe = (new cat_sat_moneda($link))->existe_by_id(registro_id: $cat_sat_moneda_id);
@@ -158,7 +159,7 @@ class base_test{
         $registro['com_tipo_cliente_id'] = $com_tipo_cliente_id;
         $registro['cat_sat_regimen_fiscal_id'] = $cat_sat_regimen_fiscal_id;
         $registro['cat_sat_uso_cfdi_id'] = $cat_sat_uso_cfdi_id;
-        $registro['id'] = 1;
+        $registro['id'] = $id;
         $registro['codigo'] = 1;
         $registro['descripcion'] = 1;
         $registro['razon_social'] = 1;
@@ -199,7 +200,19 @@ class base_test{
         }
 
         if(!$existe) {
-            $alta = (new base_test())->alta_com_cliente($link);
+            $alta = (new base_test())->alta_com_cliente(link: $link, id: $com_cliente_id);
+            if(errores::$error){
+                return (new errores())->error('Error al insertar', $alta);
+            }
+        }
+
+        $existe = (new com_tipo_sucursal($link))->existe_by_id(registro_id: $com_tipo_sucursal_id);
+        if(errores::$error){
+            return (new errores())->error('Error al verificar si existe', $existe);
+        }
+
+        if(!$existe) {
+            $alta = (new base_test())->alta_com_tipo_sucursal(link: $link, id: $com_tipo_sucursal_id);
             if(errores::$error){
                 return (new errores())->error('Error al insertar', $alta);
             }
@@ -253,6 +266,23 @@ class base_test{
 
 
         $alta = (new com_tipo_cliente($link))->alta_registro($registro);
+        if(errores::$error){
+            return (new errores())->error('Error al dar de alta ', $alta);
+
+        }
+        return $alta;
+    }
+
+    public function alta_com_tipo_sucursal(PDO $link, int $id = 1): array|\stdClass
+    {
+
+        $registro = array();
+        $registro['id'] = $id;
+        $registro['codigo'] = 1;
+        $registro['descripcion'] = 1;
+
+
+        $alta = (new com_tipo_sucursal($link))->alta_registro($registro);
         if(errores::$error){
             return (new errores())->error('Error al dar de alta ', $alta);
 
