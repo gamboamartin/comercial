18402e2b8d9f32fecefe5330a5cf1c3178d6cd8a
mgamboa <gamboa.martin@gmail.com>
2023-03-03 11:16:17 -0600

    v4.31.3 Se integra com cliente in reg fisc

--- controllers/controlador_cat_sat_regimen_fiscal.php
+++ controllers/controlador_cat_sat_regimen_fiscal.php
@@ -9,4 +9,14 @@
 namespace gamboamartin\comercial\controllers;
 
 
-class controlador_cat_sat_regimen_fiscal extends \gamboamartin\cat_sat\controllers\controlador_cat_sat_regimen_fiscal {}
+use gamboamartin\template\html;
+use PDO;
+use stdClass;
+
+class controlador_cat_sat_regimen_fiscal extends \gamboamartin\cat_sat\controllers\controlador_cat_sat_regimen_fiscal {
+    public function __construct(PDO $link, html $html = new \gamboamartin\template_1\html(), stdClass $paths_conf = new stdClass())
+    {
+        parent::__construct(link: $link,html:  $html,paths_conf:  $paths_conf);
+        $this->childrens_data['com_cliente']['title'] = 'Cliente';
+    }
+}
