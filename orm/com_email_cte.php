c6b304612b45dd2a5d5437634f8f533c77c25829
mgamboa <gamboa.martin@gmail.com>
2023-05-02 14:46:43 -0600

    v9.17.3 Se integra val email cte

--- orm/com_email_cte.php
+++ orm/com_email_cte.php
@@ -12,7 +12,7 @@ class com_email_cte extends _modelo_parent{
         $columnas = array($tabla=>false,'com_cliente'=>$tabla);
         $campos_obligatorios = array();
 
-        $tipo_campos['descripcion'] = 'email';
+        $tipo_campos['descripcion'] = 'correo';
 
         parent::__construct(link: $link, tabla: $tabla, campos_obligatorios: $campos_obligatorios,
             columnas: $columnas, tipo_campos: $tipo_campos, childrens: $childrens);
