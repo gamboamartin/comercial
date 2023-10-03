7ace850066dcc90d186276663a5cc0ba77a3f476
mgamboa <gamboa.martin@gmail.com>
2023-10-03 10:22:16 -0600

    v18.25.0 Se integran telefonos lista

--- orm/com_tels_agente.php
+++ orm/com_tels_agente.php
@@ -6,7 +6,7 @@ use PDO;
 class com_tels_agente extends _modelo_parent{
     public function __construct(PDO $link, array $childrens = array()){
         $tabla = 'com_tels_agente';
-        $columnas = array($tabla=>false,'com_tipo_tel'=>$tabla,'com_agente'=>$tabla);
+        $columnas = array($tabla=>false,'com_tipo_tel'=>$tabla,'com_agente'=>$tabla,'com_tipo_agente'=>'com_agente');
         $campos_obligatorios = array('com_tipo_tel_id','com_agente_id');
 
         $columnas_extra = array();
