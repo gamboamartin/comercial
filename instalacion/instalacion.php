991b8e2a63b0b08ed8afa0a2e998a906c45e44ad
mgamboa <gamboa.martin@gmail.com>
2024-01-16 13:36:17 -0600

    v21.1.0 Se integra instalacion en comercial

---
+++ instalacion/instalacion.php
@@ -0,0 +1,33 @@
+<?php
+namespace gamboamartin\comercial\instalacion;
+
+use gamboamartin\administrador\models\_instalacion;
+use gamboamartin\errores\errores;
+use PDO;
+
+class instalacion
+{
+    final public function instala(PDO $link)
+    {
+        $init = (new _instalacion(link: $link));
+        $foraneas = array();
+        $foraneas[] = 'dp_calle_pertenece_id';
+        $foraneas[] = 'cat_sat_regimen_fiscal_id';
+        $foraneas[] = 'cat_sat_moneda_id';
+        $foraneas[] = 'cat_sat_forma_pago_id';
+        $foraneas[] = 'cat_sat_metodo_pago_id';
+        $foraneas[] = 'cat_sat_uso_cfdi_id';
+        $foraneas[] = 'cat_sat_tipo_de_comprobante_id';
+        $foraneas[] = 'com_tipo_cliente_id';
+
+        $result = $init->foraneas(foraneas: $foraneas,table:  'com_cliente');
+
+        if(errores::$error){
+            return (new errores())->error(mensaje: 'Error al ajustar foranea', data:  $result);
+        }
+
+        return $result;
+
+    }
+
+}
