3e552b9953a815539f13b6ce139be538a344ea08
mgamboa <gamboa.martin@gmail.com>
2024-01-25 14:47:51 -0600

    v22.10.0 Se inetgran elemenots de fron dom en upd

--- tests/instalacion/instalacionTest.php
+++ tests/instalacion/instalacionTest.php
@@ -26,13 +26,23 @@ class instalacionTest extends test {
 
     public function test_instala(): void
     {
-        errores::$error = false;
 
         $_GET['seccion'] = 'cat_sat_tipo_persona';
         $_GET['accion'] = 'lista';
         $_SESSION['grupo_id'] = 1;
         $_SESSION['usuario_id'] = 2;
         $_GET['session_id'] = '1';
+
+        $init_cat_sat = (new \gamboamartin\cat_sat\instalacion\instalacion())->instala(link: $this->link);
+
+        if(errores::$error){
+            $error = (new errores())->error('Error al del', $init_cat_sat);
+            print_r($error);
+            exit;
+        }
+        errores::$error = false;
+
+
         $ins = new instalacion($this->link);
         //$modelo = new liberator($modelo);
 
