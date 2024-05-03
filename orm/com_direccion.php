e3716622bdfeebc98b5a56e16a363a1272055ba2
Kevin <kevin.acuna@ivitec.com.mx>
2024-05-03 13:34:47 -0500

    v33.5.0 Se integra direcciones en com.

---
+++ orm/com_direccion.php
@@ -0,0 +1,24 @@
+<?php
+namespace gamboamartin\comercial\models;
+use base\orm\_modelo_parent;
+use gamboamartin\administrador\models\adm_usuario;
+use gamboamartin\errores\errores;
+use PDO;
+use stdClass;
+
+class com_direccion extends _modelo_parent{
+    public function __construct(PDO $link, array $childrens = array()){
+        $tabla = 'com_direccion ';
+        $columnas = array($tabla=>false,'dp_calle_pertenece'=>$tabla);
+
+        $campos_obligatorios = array('dp_calle_pertenece');
+
+        parent::__construct(link: $link, tabla: $tabla, campos_obligatorios: $campos_obligatorios,
+            columnas: $columnas, childrens: $childrens);
+
+        $this->NAMESPACE = __NAMESPACE__;
+
+        $this->etiqueta = 'Agentes';
+    }
+
+}
---
+++ templates/inputs/com_direccion/alta.php
@@ -0,0 +1,14 @@
+<?php /** @var \gamboamartin\comercial\models\com_direccion $controlador  controlador en ejecucion */ ?>
+<?php use config\views; ?>
+<?php echo $controlador->inputs->dp_calle_id; ?>
+
+
+<?php echo $controlador->inputs->texto_exterior; ?>
+<?php echo $controlador->inputs->texto_interior; ?>
+
+<?php include (new views())->ruta_templates.'botons/submit/alta_bd.php';?>
+
+
+
+
+
