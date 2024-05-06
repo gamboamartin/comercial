fe83bcbd9b893ed21ee104ca3d3c55929c7fc57b
Kevin <kevin.acuna@ivitec.com.mx>
2024-05-06 14:48:32 -0500

    v33.12.2 Se integra relacion

---
+++ orm/com_direccion_prospecto.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace gamboamartin\comercial\models;
+
+use base\orm\_modelo_parent;
+use base\orm\modelo;
+use gamboamartin\errores\errores;
+use gamboamartin\gastos\models\_base_auto_soli;
+use PDO;
+use stdClass;
+
+class com_direccion_prospecto extends _modelo_parent
+{
+    public function __construct(PDO $link)
+    {
+        $tabla = 'gt_cotizadores';
+        $columnas = array($tabla => false, 'com_direccion' => $tabla, 'com_prospecto' => $tabla);
+        $campos_obligatorios = array();
+
+        $no_duplicados = array();
+
+        parent::__construct(link: $link, tabla: $tabla, campos_obligatorios: $campos_obligatorios,
+            columnas: $columnas, no_duplicados: $no_duplicados);
+
+        $this->NAMESPACE = __NAMESPACE__;
+    }
+
+    public function alta_bd(array $keys_integra_ds = array('codigo', 'descripcion')): array|stdClass
+    {
+        $this->registro = $this->inicializa_campos($this->registro);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al inicializar campo base', data: $this->registro);
+        }
+
+        $r_alta_bd = parent::alta_bd($keys_integra_ds);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al insertar autorizante', data: $r_alta_bd);
+        }
+        return $r_alta_bd;
+    }
+
+    protected function inicializa_campos(array $registros): array
+    {
+        $keys = array('com_direccion_id', 'com_prospecto_id');
+        $valida = $this->validacion->valida_ids(keys: $keys, registro: $registros);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error validar campos', data: $valida);
+        }
+
+        $registros['codigo'] = $this->get_codigo_aleatorio();
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error generar codigo', data: $registros);
+        }
+
+        $registros['codigo'] .= $registros['gt_cotizador_id'];
+        $registros['descripcion'] = $registros['codigo'];
+
+        return $registros;
+    }
+
+}
